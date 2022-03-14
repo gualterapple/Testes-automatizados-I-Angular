6a0f5a666a999633c2a7e1db9189025c06e01ec7
gualterapple <39972000+gualterapple@users.noreply.github.com>
2022-03-14 13:30:57 +0000

    Utilizando o BeforeEach

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,20 +1,32 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
+
+  let service = null;
+
+  beforeEach(()=>{
+    service = new UniqueIdService();
+  });
+
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
 
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate ids when called multiple times`, () => {
-    const service = new UniqueIdService();
     const ids = new Set();
     for(let i = 0; i<=50; i++)
     {
       ids.add(service.generateUniqueIdWithPrefix('app'));
     }
     expect(ids.size).not.toBe(50);
+
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedIds.name} should return the number of generatedId when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedIds()).toBe(2);
 
   });
 
